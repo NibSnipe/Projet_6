d976041037217dcf99d18aa6a8efce30a2e88c1a
maoly <maoly.bissemo@gmail.com>
2024-11-19 22:35:03 +0100

    erreur Cors corrigé

--- app.js
+++ app.js
@@ -19,6 +19,19 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.error("Connexion à MongoDB échouée : ", error));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/books", require("./routes/books"));
 app.use("/api/auth", require("./routes/users"));
 
